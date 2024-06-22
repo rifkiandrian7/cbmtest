82b4e0bf9c7ba38b254333ad6991ff681ae1399f
Rifki Andriantono <rifkiandriantono839@gmail.com>
2024-06-22 14:32:01 +0700

    Added table -wo summary

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Untuk menampilkan dan menyembunyikan icon mata di password input
 function togglePassword() {
     const passwordInput = document.getElementById('passwordInput');
     const passwordIcon = document.getElementById('passwordIcon');
@@ -13,6 +15,7 @@ function togglePassword() {
     }
 }
 
+// Fungsi untuk menu tab, menampilkan dan menyembunyikan konten
 document.addEventListener('DOMContentLoaded', function() {
     var triggerTabList = [].slice.call(document.querySelectorAll('#nav-tab a'))
     triggerTabList.forEach(function(triggerEl) {
@@ -24,4 +27,25 @@ document.addEventListener('DOMContentLoaded', function() {
       })
     })
 })
+
+// Fungsi untuk menggeser baris pada tabel
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('.move-up').forEach(button => {
+    button.addEventListener('click', () => {
+      let row = button.closest('tr');
+      if (row.previousElementSibling && row.previousElementSibling.tagName === 'TR') {
+        row.parentNode.insertBefore(row, row.previousElementSibling);
+      }
+    });
+  });
+
+  document.querySelectorAll('.move-down').forEach(button => {
+    button.addEventListener('click', () => {
+      let row = button.closest('tr');
+      if (row.nextElementSibling && row.nextElementSibling.tagName === 'TR') {
+        row.parentNode.insertBefore(row.nextElementSibling, row);
+      }
+    });
+  });
+});
 
